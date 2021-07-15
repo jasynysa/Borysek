9924cddd4deb5499cea5e3cc75deea2eb2d32990
jasy <jasynysa@gmail.com>
2021-07-15 21:30:02 +0200

    NameMenager class finished.

--- 10/Parser/NameMenager.h
+++ 10/Parser/NameMenager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Token.h"
 #include<map>
+#include <string>
+using namespace std;
 class NameMenager
 {
 public:
@@ -9,7 +11,8 @@ public:
 
 	}
 
-	bool isItVar(Token t)
+	//return true if token is identifier and hasn t been useed as var name
+	bool isItVarNewName(Token t)
 	{
 		if (t.token == identifier)
 		{
@@ -22,31 +25,62 @@ public:
 		return false;
 	}
 
-	int getVarNumber(string varName)
+	//return true if token is identifier and hasn t been useed as subroutine name
+	bool isItSubrutineNewName(Token t)
 	{
-
+		if (t.token == identifier)
+		{
+			if (mapSubroutinesNames.count(t.value) == 0)
+			{
+				mapVarNames.emplace(t.value, varCount++);
+				return true;
+			}
+		}
+		return false;
 	}
 
-	bool isItSubrutine(EnumToken t)
+	//return true if tokens are identifier and var name hasn t been useed as given soubroutine's var name
+	bool isItSubroutinesVarName(Token functionName, Token varName)
 	{
-
+		if (functionName.token == identifier && varName.token == identifier)
+		{
+			Subroutine s = mapSubroutinesNames.at(functionName.value);
+			if (s.mapSubroutineVarsName.count(varName.value) == 0)
+			{
+				s.mapSubroutineVarsName.emplace(varName.value, s.varCount++);
+				return true;
+			}
+		}
+		return false;
 	}
 
-	bool isItSubroutinesVar(EnumToken functionName, EnumToken varname)
+	//return var index depending on whether it is class variable or subroutine variable
+	int getVarIndex(string subroutineName , string varName)
 	{
-
+		Subroutine s = mapSubroutinesNames.at(subroutineName);
+		int index;
+		try
+		{
+			return s.mapSubroutineVarsName.at(varName);			
+		}
+		catch (const std::exception & e)
+		{
+			return mapVarNames.at(varName);
+		}
+		throw exception(((string)"cant find variable named: '" + varName + "'").c_str());
 	}
-
 private:
 	int varCount = 0;
-	map<string, int> mapVarNames;	
+	std::map<string, int> mapVarNames;	
 
 	struct Subroutine
 	{
 		EnumToken type;
 		map<string, int> mapSubroutineVarsName;
+		int varCount = 0;
 	};
 
-	map<string, Subroutine> mapSubroutines;
+	map<string, Subroutine> mapSubroutinesNames;
+
 };
 
